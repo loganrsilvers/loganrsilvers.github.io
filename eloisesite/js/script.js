98100846e8af0321a708e04d8327173b5a7396dd
Logan Silvers <loganrsilvers@gmail.com>
2025-10-16 09:31:20 -0500

    about and side nav

--- eloisesite/js/script.js
+++ eloisesite/js/script.js
@@ -1,4 +1,25 @@
-let elements = document.getElementsByClassName("column");
-function one() { for (let c of elements) { c.style.flex = "100%"; c.style.maxWidth = "100%"; } }
-function two() { for (let c of elements) { c.style.flex = "50%"; c.style.maxWidth = "50%"; } }
-function four() { for (let c of elements) { c.style.flex = "25%"; c.style.maxWidth = "25%"; } }
+const allContainers = document.querySelectorAll('.image-grid .img-container');
+let expandedImg = null;
+
+allContainers.forEach(container => {
+  const img = container.querySelector('img');
+  img.addEventListener('click', function() {
+    if (expandedImg && expandedImg !== container) {
+      expandedImg.classList.remove('expanded-img');
+    }
+    if (container.classList.contains('expanded-img')) {
+      container.classList.remove('expanded-img');
+      expandedImg = null;
+    } else {
+      container.classList.add('expanded-img');
+      expandedImg = container;
+    }
+  });
+});
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
